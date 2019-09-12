3d81ad959a4d0726a3e6fd5038f53f3e5bd9bb88
chuyang <511990372@qq.com>
2019-09-12 18:21:15 +0800

    功能性页面开发完毕。

--- src/main/java/com/cy/xunwu/web/controller/HomeController.java
+++ src/main/java/com/cy/xunwu/web/controller/HomeController.java
@@ -25,4 +25,24 @@ public class HomeController {
         return ApiResponse.ofMessage(200, "成功了");
     }
 
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout/page")
+    public String logoutPage() {
+        return "logout";
+    }
+
 }
